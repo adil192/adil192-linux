705a95eb470734415d53652ecf3ffa11dcb742bd
Adil Hanney <adilhanney@disroot.org>
2025-04-07 19:38:09 +0100

    Fix: Better gpu detection

--- bin/src/tools/device.dart
+++ bin/src/tools/device.dart
@@ -6,33 +6,42 @@ import 'result_of_command.dart';
 final class Device {
   static Future<bool> hasAmdCpu() async {
     cpuInfo ??= await _getCpuInfo();
-    return cpuInfo!.contains('AMD');
+    return cpuInfo!.toLowerCase().contains('amd');
   }
 
   static Future<bool> hasIntelCpu() async {
     cpuInfo ??= await _getCpuInfo();
-    return cpuInfo!.contains('Intel');
+    return cpuInfo!.toLowerCase().contains('intel');
   }
 
   static Future<bool> hasAmdGpu() async {
     gpuInfo ??= await _getGpuInfo();
-    return gpuInfo!.contains('AMD');
+    return gpuInfo!.toLowerCase().contains('amd');
   }
 
   static Future<bool> hasIntelGpu() async {
     gpuInfo ??= await _getGpuInfo();
-    return gpuInfo!.contains('Intel');
+    return gpuInfo!.toLowerCase().contains('intel');
   }
 
   static Future<bool> hasNvidiaGpu() async {
     gpuInfo ??= await _getGpuInfo();
-    return gpuInfo!.contains('NVIDIA');
+    return gpuInfo!.toLowerCase().contains('nvidia');
   }
 
   static String? cpuInfo;
   static Future<String> _getCpuInfo() => File('/proc/cpuinfo').readAsString();
 
   static String? gpuInfo;
-  static Future<String> _getGpuInfo() =>
-      resultOfCommand('glxinfo', ['-B'], silent: true);
+  static Future<String> _getGpuInfo() async {
+    // lspci | egrep -i "vga|display|3d"
+    final lspci = await resultOfCommand('lspci', ['-mm'], silent: true);
+    return lspci
+        .split('\n')
+        .where((line) =>
+            line.toLowerCase().contains('vga') ||
+            line.toLowerCase().contains('display') ||
+            line.toLowerCase().contains('3d'))
+        .join('\n');
+  }
 }
