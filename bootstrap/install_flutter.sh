9746a0d5295bdeff69e56ebb0844287936769cdb
Adil Hanney <adilhanney@disroot.org>
2025-04-20 21:05:42 +0100

    Fix: Install more linux flutter dependencies
    [✗] Linux toolchain - develop for Linux desktop
        ✗ clang++ is required for Linux development.
          It is likely available from your distribution (e.g.: apt install clang), or can be downloaded from https://releases.llvm.org/
        ✗ CMake is required for Linux development.
          It is likely available from your distribution (e.g.: apt install cmake), or can be downloaded from https://cmake.org/download/
        ✗ ninja is required for Linux development.
          It is likely available from your distribution (e.g.: apt install ninja-build), or can be downloaded from
          https://github.com/ninja-build/ninja/releases

--- bootstrap/install_flutter.sh
+++ bootstrap/install_flutter.sh
@@ -4,10 +4,10 @@ FLUTTER_DIR="$HOME/Documents/Sources/flutter/"
 
 echo "Installing Flutter's dependencies..."
 if [ -n "$(which dnf)" ]; then
-  sudo dnf install -y curl git unzip xz zip libGLU
+  sudo dnf install -y curl git unzip xz zip libGLU clang cmake ninja-build
 elif [ -n "$(which apt)" ]; then
   sudo apt update
-  sudo apt install -y curl git unzip xz-utils zip libglu1-mesa
+  sudo apt install -y curl git unzip xz-utils zip libglu1-mesa clang cmake ninja-build
 else
   echo "Can't find apt or dnf. Please install dependencies manually."
 fi
