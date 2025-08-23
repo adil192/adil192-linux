387038da228fcc068e934047b3e1c0567db6212f
Adil Hanney <adilhanney@disroot.org>
2025-08-23 12:45:42 +0100

    fix: create .config gtk folders if needed

--- bin/src/gtk.dart
+++ bin/src/gtk.dart
@@ -36,6 +36,7 @@ Future<void> uninstallGtkCss() async {
 /// /* END adil192-linux gtk.css */
 /// is replaced with the new custom CSS.
 Future<void> _injectCss(File file, String customCssContent) async {
+  await file.parent.create(recursive: true);
   final content = file.existsSync() ? await file.readAsString() : '';
 
   const start = '/* START adil192-linux gtk.css */';
