f89b7ef80764df00c3bb1aac23f25d2e5ba267c8
Adil Hanney <adilhanney@disroot.org>
2026-02-04 16:11:24 +0000

    ref: use ArgParser instead of manual parsing

--- bin/adil192_linux.dart
+++ bin/adil192_linux.dart
@@ -1,3 +1,5 @@
+import 'package:args/args.dart';
+
 import 'src/tools/yes_or_no.dart';
 import 'src/config.dart';
 import 'src/firefox_cache.dart';
@@ -6,11 +8,37 @@ import 'src/install_apps.dart';
 import 'src/install_codecs.dart';
 
 Future<void> main(List<String> args) async {
-  final noInteraction = args.contains('--no-interaction');
-  alwaysYes = args.contains('-y') || args.contains('--yes');
+  final parsedArgs = _argParser.parse(args);
+  if (parsedArgs.flag('help')) {
+    print(_argParser.usage);
+    return;
+  }
+
+  final noInteraction = parsedArgs.flag('no-interaction');
+  alwaysYes = parsedArgs.flag('yes');
 
   if (shouldInstallCodecs && !noInteraction) await installCodecs();
   if (shouldInstallApps && !noInteraction) await installApps();
   if (shouldThemeFirefox) await installFirefoxCss();
   if (shouldInstallFirefoxCacher) await installFirefoxCacher();
 }
+
+final _argParser = ArgParser()
+  ..addFlag(
+    'no-interaction',
+    abbr: 'n',
+    negatable: false,
+    help: 'Skip any steps needing confirmation',
+  )
+  ..addFlag(
+    'yes',
+    abbr: 'y',
+    negatable: false,
+    help: 'Always answer yes to prompts',
+  )
+  ..addFlag(
+    'help',
+    abbr: 'h',
+    negatable: false,
+    help: 'Show this help message',
+  );
