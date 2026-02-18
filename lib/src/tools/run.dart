3d7cf8f07543e652833984a5767d5290ce4d5d4d
Adil Hanney <adilhanney@disroot.org>
2026-02-18 01:14:13 +0000

    chore: remove nonfunctional stdout return

--- lib/src/tools/run.dart
+++ lib/src/tools/run.dart
@@ -1,20 +1,19 @@
-import 'dart:convert';
 import 'dart:io';
 
-/// Runs the command and returns the output as a string.
+/// Runs the command with stdio, as if we're just running it normally
+/// in a terminal app.
 ///
 /// Notes:
 /// - Throws if the exit code is non-zero.
 /// - Process inherits stdio: it prints to console and can take user input.
-Future<String> run(String command, List<String> args) async {
+/// - This function does not return stdout.
+Future<void> run(String command, List<String> args) async {
   final process = await Process.start(command, args, mode: .inheritStdio);
 
   final exitCode = await process.exitCode;
   if (exitCode != 0) {
     throw StateError('$command command failed with exit code $exitCode');
   }
-
-  return process.stdout.transform(utf8.decoder).join();
 }
 
 /// Runs the command and returns the output as a string.
@@ -23,6 +22,7 @@ Future<String> run(String command, List<String> args) async {
 /// - Throws if the exit code is non-zero.
 /// - Process does not inherit stdio:
 ///   Nothing is printed to console and no user input can be taken.
+/// - This function returns stdout.
 String runSilent(String command, List<String> args) {
   final result = Process.runSync(command, args);
 
