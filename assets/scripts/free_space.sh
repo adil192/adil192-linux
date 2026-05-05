e53e9e0664021b48b219fa5f00682a0bcbff72fc
Adil Hanney <adilhanney@disroot.org>
2026-05-05 12:49:14 +0100

    feat: prune unused Dart packages

--- assets/scripts/free_space.sh
+++ assets/scripts/free_space.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Remove temporary development files
 for dir in ~/Documents/{GitHub,Sources}/{*,*/*}/{.flatpak-builder,builddir,build,dist,node_modules,target}; do
   if [[ -d "$dir" ]]; then
     read -p "Delete $dir? (y/N) "
@@ -8,3 +9,8 @@ for dir in ~/Documents/{GitHub,Sources}/{*,*/*}/{.flatpak-builder,builddir,build
     fi
   fi
 done
+
+# Prune unused Dart packages
+if command -v dart; then
+  dart pub cache gc
+fi
