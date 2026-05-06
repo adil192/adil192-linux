d4c1ef5567ebd8b0c7bfa101714714bf288bd66d
Adil Hanney <adilhanney@disroot.org>
2026-05-06 12:37:58 +0100

    feat: add script to install frosted glass for COSMIC

---
+++ assets/scripts/install_cosmic_blur.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+mkdir -p ~/Documents/GitHub/ && cd ~/Documents/GitHub/
+
+if [ "$EUID" -eq 0 ]; then
+  echo "Run this script as a regular user, not as root!"
+  exit 1
+fi
+
+echo "This installs UNRELEASED WIP versions of critical system components."
+echo "Frosted glass is really cool, but don't expect support if you run into issues!"
+echo
+
+echo "Please grant sudo access to install system components."
+sudo echo "sudo granted!"
+echo
+
+echo "Starting the builds. This will take a while..."
+echo "If the builds fail, ensure you have the necessary dependencies: https://github.com/pop-os/cosmic-epoch#setup-on-distributions-without-packaging-of-cosmic-components"
+
+install() {
+  PROJECT=$1
+  BRANCH=$2
+  [ -d "$PROJECT" ] || git clone "https://github.com/pop-os/$PROJECT.git"
+  pushd "$PROJECT"
+  git fetch && git switch "$BRANCH" && git pull
+  if [ -f Makefile ]; then
+    make && sudo make install
+  else
+    just && sudo just install
+  fi
+  popd
+}
+
+install cosmic-app-library theme-v2
+install cosmic-applets theme-v2
+install cosmic-comp frosted-glass_noble
+install cosmic-edit theme-v2
+install cosmic-files theme-v2
+install cosmic-launcher theme-v2
+install cosmic-panel theme-v2
+install cosmic-settings theme-v2
+install cosmic-store theme-v2
+install cosmic-term theme-v2
+install cosmic-launcher theme-v2
