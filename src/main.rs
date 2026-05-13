fdb7fce78f1ee57852ee23ede2a83f45dd0e480e
Adil Hanney <adilhanney@disroot.org>
2026-05-13 13:56:34 +0100

    ref: remove args
    I'm making everything interactive now so CLI args aren't needed

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
 mod apps;
-mod args;
 mod drivers;
 mod firefox_cacher;
 mod tools;
 
-use clap::Parser;
-
 use crate::{apps::MyApps, drivers::MyDrivers, firefox_cacher::FirefoxCacher};
 
 fn main() -> anyhow::Result<()> {
-  let args = args::Args::parse();
-
-  if args.install_drivers {
-    MyDrivers::install()?;
-  }
-  if args.install_apps {
-    MyApps::install()?;
-  }
-  if args.install_firefox_cacher {
-    FirefoxCacher::install()?;
-  }
+  MyDrivers::install()?;
+  MyApps::install()?;
+  FirefoxCacher::install()?;
 
   Ok(())
 }
