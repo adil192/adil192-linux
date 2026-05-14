c916af096f6bbc078260881e679da94e529fe28d
Adil Hanney <adilhanney@disroot.org>
2026-05-14 16:25:01 +0100

    feat: use my fork of cosmic-settings-daemon
    I switched to a blue theme in theme-v2 but my theme-v1 is still green. This change is required to switch gtk/qt apps over to my new blue theme.

--- assets/scripts/install_cosmic_blur.sh
+++ assets/scripts/install_cosmic_blur.sh
@@ -44,11 +44,19 @@ clone cosmic-workspaces-epoch theme-v2
 clone libcosmic theme-v2
 clone xdg-desktop-portal-cosmic theme-v2
 
+# There isn't an official theme-v2 branch of cosmic-settings-daemon yet so use mine.
+# Without this, gtk/qt apps will be stuck on whatever theme you had before installing theme-v2.
+[[ -d "cosmic-settings-daemon" ]] || git clone "https://github.com/pop-os/cosmic-settings-daemon.git" --recurse-submodules
+git -C "cosmic-settings-daemon" remote add adil192 "https://github.com/adil192/cosmic-settings-daemon.git" || true
+git -C "cosmic-settings-daemon" fetch adil192
+git -C "cosmic-settings-daemon" switch theme-v2-unofficial
+git -C "cosmic-settings-daemon" pull
+
 echo
 echo "Starting the builds. This will take a while..."
 echo "If the builds fail, ensure you have the necessary dependencies: https://github.com/pop-os/cosmic-epoch#setup-on-distributions-without-packaging-of-cosmic-components"
 echo
-sleep 1
+sleep 3
 
 # Suppress warnings since they flood the logs
 export RUSTFLAGS=-Awarnings
@@ -72,6 +80,7 @@ install cosmic-launcher
 install cosmic-osd
 install cosmic-panel
 install cosmic-settings
+install cosmic-settings-daemon
 install cosmic-store
 install cosmic-term
 install cosmic-workspaces-epoch
