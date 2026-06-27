1a6278941aae98109543cc007717664005cd51db
Adil Hanney <adilhanney@disroot.org>
2026-06-27 12:15:00 +0100

    feat: use cosmic kcolorscheme on gnome

--- assets/scripts/switch_gnome_theme.sh
+++ assets/scripts/switch_gnome_theme.sh
@@ -5,12 +5,16 @@ if [ "$1" == "dark" ]; then
   dconf write /org/gnome/desktop/interface/cursor-theme "'Yaru'"
   dconf write /org/gnome/desktop/interface/gtk-theme "'Yaru-prussiangreen-dark'"
   dconf write /org/gnome/desktop/interface/icon-theme "'Yaru-prussiangreen-dark'"
+  KCOLORSCHEME="$HOME/.local/share/color-schemes/CosmicDark.colors"
+  [ -f "$KCOLORSCHEME" ] || KCOLORSCHEME="/usr/share/color-schemes/BreezeDark.colors"
   sed -i \
-    -e 's|^color_scheme_path=/usr/share/color-schemes/Breeze.*|color_scheme_path=/usr/share/color-schemes/BreezeDark.colors|' \
+    -e "s|^color_scheme_path=/usr/share/color-schemes/Breeze.*|color_scheme_path=$KCOLORSCHEME|" \
+    -e "s|^color_scheme_path=$HOME/.local/share/color-schemes/Cosmic.*|color_scheme_path=$KCOLORSCHEME|" \
     -e 's|^icon_theme=breez.*|icon_theme=breeze-dark|' \
     ~/.config/qt6ct/qt6ct.conf
   sed -i \
-    -e 's|^color_scheme_path=/usr/share/color-schemes/Breeze.*|color_scheme_path=/usr/share/color-schemes/BreezeDark.colors|' \
+    -e "s|^color_scheme_path=/usr/share/color-schemes/Breeze.*|color_scheme_path=$KCOLORSCHEME|" \
+    -e "s|^color_scheme_path=$HOME/.local/share/color-schemes/Cosmic.*|color_scheme_path=$KCOLORSCHEME|" \
     -e 's|^icon_theme=breez.*|icon_theme=breeze-dark|' \
     ~/.config/qt5ct/qt5ct.conf
 elif [ "$1" == "light" ]; then
@@ -19,12 +23,16 @@ elif [ "$1" == "light" ]; then
   dconf write /org/gnome/desktop/interface/cursor-theme "'Yaru'"
   dconf write /org/gnome/desktop/interface/gtk-theme "'Yaru-prussiangreen'"
   dconf write /org/gnome/desktop/interface/icon-theme "'Yaru-prussiangreen'"
+  KCOLORSCHEME="$HOME/.local/share/color-schemes/CosmicLight.colors"
+  [ -f "$KCOLORSCHEME" ] || KCOLORSCHEME="/usr/share/color-schemes/BreezeLight.colors"
   sed -i \
-    -e 's|^color_scheme_path=/usr/share/color-schemes/Breeze.*|color_scheme_path=/usr/share/color-schemes/BreezeLight.colors|' \
+    -e "s|^color_scheme_path=/usr/share/color-schemes/Breeze.*|color_scheme_path=$KCOLORSCHEME|" \
+    -e "s|^color_scheme_path=$HOME/.local/share/color-schemes/Cosmic.*|color_scheme_path=$KCOLORSCHEME|" \
     -e 's|^icon_theme=breez.*|icon_theme=breeze|' \
     ~/.config/qt6ct/qt6ct.conf
   sed -i \
-    -e 's|^color_scheme_path=/usr/share/color-schemes/Breeze.*|color_scheme_path=/usr/share/color-schemes/BreezeLight.colors|' \
+    -e "s|^color_scheme_path=/usr/share/color-schemes/Breeze.*|color_scheme_path=$KCOLORSCHEME|" \
+    -e "s|^color_scheme_path=$HOME/.local/share/color-schemes/Cosmic.*|color_scheme_path=$KCOLORSCHEME|" \
     -e 's|^icon_theme=breez.*|icon_theme=breeze|' \
     ~/.config/qt5ct/qt5ct.conf
 else
