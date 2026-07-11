b24a626accd70dc145c713dce722950b55aaf691
Adil Hanney <adilhanney@disroot.org>
2026-07-12 00:42:42 +0100

    fix: use double quotes to prevent shell splitting

--- scripts/sunshine-res.sh
+++ scripts/sunshine-res.sh
@@ -14,11 +14,11 @@ set -e
 # It is deleted after being restored to avoid using stale state in the future.
 # When present, this SAVE_FILE is better than FALLBACK_SAVE_FILE since the
 # latter may be outdated.
-SAVE_FILE="$(dirname $0)/.sunshine-res.backup.tmp"
+SAVE_FILE="$(dirname "$0")/.sunshine-res.backup.tmp"
 # A fallback file used in "undo" if SAVE_FILE isn't present.
 # It is only created once on first use and is not deleted after restore.
 # This provides a fallback when a newer fresher SAVE_FILE is not available.
-FALLBACK_SAVE_FILE="$(dirname $0)/.sunshine-res.backup.fallback"
+FALLBACK_SAVE_FILE="$(dirname "$0")/.sunshine-res.backup.fallback"
 
 if [ "$1" == "do" ]; then
   # Make a backup to restore later in "undo"
@@ -33,10 +33,10 @@ if [ "$1" == "do" ]; then
   # Set resolution from sunshine on DP-1
   displayconfig-mutter set \
     --connector DP-1 \
-    --resolution ${SUNSHINE_CLIENT_WIDTH:-1280}x${SUNSHINE_CLIENT_HEIGHT:-720} \
-    --refresh-rate ${SUNSHINE_CLIENT_FPS:-30} \
+    --resolution "${SUNSHINE_CLIENT_WIDTH:-1280}x${SUNSHINE_CLIENT_HEIGHT:-720}" \
+    --refresh-rate "${SUNSHINE_CLIENT_FPS:-30}" \
     --vrr false \
-    --hdr ${SUNSHINE_CLIENT_HDR:-false}
+    --hdr "${SUNSHINE_CLIENT_HDR:-false}"
 elif [ "$1" == "undo" ]; then
   # Restore backed up monitor configuration
   if [ -f "$SAVE_FILE" ]; then
@@ -46,6 +46,6 @@ elif [ "$1" == "undo" ]; then
     displayconfig-mutter load-file "$FALLBACK_SAVE_FILE"
   fi
 else
-  echo "Unknown option '$1'."
+  echo "Unknown option '$1'. Must be do or undo."
   exit 1
 fi
