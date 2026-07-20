097b79807f5221fbe949d0fb78218b09d6c645ab
Adil Hanney <adilhanney@disroot.org>
2026-07-20 17:23:45 +0100

    ref: simplify switch_gnome_theme.sh with ternaries

--- scripts/switch_gnome_theme.sh
+++ scripts/switch_gnome_theme.sh
@@ -1,39 +1,27 @@
 #!/bin/bash
-if [ "$1" == "dark" ]; then
-  dconf write /org/gnome/shell/extensions/user-theme/name "'Yaru-dark'"
-  dconf write /org/gnome/desktop/interface/accent-color "'teal'"
-  dconf write /org/gnome/desktop/interface/cursor-theme "'Breeze_Light'"
-  dconf write /org/gnome/desktop/interface/gtk-theme "'Yaru-prussiangreen-dark'"
-  dconf write /org/gnome/desktop/interface/icon-theme "'Yaru-prussiangreen-dark'"
-  KCOLORSCHEME="$HOME/.local/share/color-schemes/CosmicDark.colors"
-  [ -f "$KCOLORSCHEME" ] || KCOLORSCHEME="/usr/share/color-schemes/BreezeDark.colors"
-  cp "$KCOLORSCHEME" "$HOME/.config/kdeglobals"
-  sed -i \
-    -e "s|^color_scheme_path=.*|color_scheme_path=$KCOLORSCHEME|" \
-    -e 's|^icon_theme=.*|icon_theme=breeze-dark|' \
-    ~/.config/qt6ct/qt6ct.conf
-  sed -i \
-    -e "s|^color_scheme_path=.*|color_scheme_path=$KCOLORSCHEME|" \
-    -e 's|^icon_theme=.*|icon_theme=breeze-dark|' \
-    ~/.config/qt5ct/qt5ct.conf
-elif [ "$1" == "light" ]; then
-  dconf write /org/gnome/shell/extensions/user-theme/name "'Yaru'"
-  dconf write /org/gnome/desktop/interface/accent-color "'teal'"
-  dconf write /org/gnome/desktop/interface/cursor-theme "'Breeze_cursors'"
-  dconf write /org/gnome/desktop/interface/gtk-theme "'Yaru-prussiangreen'"
-  dconf write /org/gnome/desktop/interface/icon-theme "'Yaru-prussiangreen'"
-  KCOLORSCHEME="$HOME/.local/share/color-schemes/CosmicLight.colors"
-  [ -f "$KCOLORSCHEME" ] || KCOLORSCHEME="/usr/share/color-schemes/BreezeLight.colors"
-  cp "$KCOLORSCHEME" "$HOME/.config/kdeglobals"
-  sed -i \
-    -e "s|^color_scheme_path=.*|color_scheme_path=$KCOLORSCHEME|" \
-    -e 's|^icon_theme=.*|icon_theme=breeze|' \
-    ~/.config/qt6ct/qt6ct.conf
-  sed -i \
-    -e "s|^color_scheme_path=.*|color_scheme_path=$KCOLORSCHEME|" \
-    -e 's|^icon_theme=.*|icon_theme=breeze|' \
-    ~/.config/qt5ct/qt5ct.conf
-else
+
+MODE="$1"
+if [[ "$MODE" != dark && "$MODE" != "light" ]]; then
   echo "Argument must be light or dark, not $1."
   exit 1
 fi
+
+light-dark() {
+  [ "$MODE" == "light" ] && echo "$1" || echo "$2"
+}
+
+gsettings set org.gnome.shell.extensions.user-theme name "$(light-dark Yaru Yaru-dark)"
+gsettings set org.gnome.desktop.interface accent-color teal
+gsettings set org.gnome.desktop.interface cursor-theme "$(light-dark Breeze_cursors Breeze_Light)"
+gsettings set org.gnome.desktop.interface gtk-theme "$(light-dark Yaru-prussiangreen Yaru-prussiangreen-dark)"
+gsettings set org.gnome.desktop.interface icon-theme "$(light-dark Yaru-prussiangreen Yaru-prussiangreen-dark)"
+
+KCOLORSCHEME="/usr/share/color-schemes/$(light-dark BreezeLight.colors BreezeDark.colors)"
+KICONTHEME=$(light-dark breeze breeze-dark)
+cp "$KCOLORSCHEME" ~/.config/kdeglobals
+for CONF in ~/.config/qt5ct/qt5ct.conf ~/.config/qt6ct/qt6ct.conf; do
+  sed -i \
+    -e "s|^color_scheme_path=.*|color_scheme_path=$KCOLORSCHEME|" \
+    -e "s|^icon_theme=.*|icon_theme=$KICONTHEME|" \
+    "$CONF"
+done
